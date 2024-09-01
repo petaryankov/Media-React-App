84a43b7cf0e6c67e61b0dc09a1a0ff8c987945bc
Petar Yankov <yankov.petar4@gmail.com>
2024-09-01 08:54:24 +0300

    Thunk created

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
 
 function UsersList() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
     return "Users list!!!"
 }
 
