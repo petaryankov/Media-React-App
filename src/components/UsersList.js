5891d8028693399b2cd216258a0dfba51151810f
Petar Yankov <yankov.petar4@gmail.com>
2024-09-03 07:47:42 +0300

    loading state added

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,14 +1,27 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
     const dispatch = useDispatch();
+    const {data, isLoading, error} = useSelector((state) => {
+        return state.users;
+    });
 
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
-    return "Users list!!!"
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if (error) {
+        return <div>Error fetching data...</div>
+    }
+    return <div>
+        {data.length}
+    </div>
 }
 
 export default UsersList;
