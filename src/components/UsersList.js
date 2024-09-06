550dbcc1d89e1747e7fa6510bd0c3778ebb677f2
Petar Yankov <yankov.petar4@gmail.com>
2024-09-06 20:32:53 +0300

    creating a reuseble thunk hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,27 +1,25 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
+import { useThunk } from "../hooks/use-thunk";
+
 
 function UsersList() {
-    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-    const [loadingUsersError, setLoadingUsersError] = useState(null);
-    const dispatch = useDispatch();
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
     const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        setIsLoadingUsers(true);
-        dispatch(fetchUsers())
-            .unwrap()
-            .catch((err) => setLoadingUsersError(err))
-            .finally(() => setIsLoadingUsers(false));
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
     const handleUserAdd = () => {
-        dispatch(addUser());
+        doCreateUser();
     }
 
     if (isLoadingUsers) {
@@ -45,9 +43,12 @@ function UsersList() {
     return <div>
         <div className="flex flex-row justify-between m-3">
             <h1 className="m-2 text-xl">Users</h1>
-            <Button onClick={handleUserAdd}>
-                + Add User
-            </Button>
+            {
+                isCreatingUser
+                    ? ("Creating User...")
+                    : (<Button onClick={handleUserAdd}>+ Add User</Button>)
+            }
+            {creatingUserError && "Error creating user..."}
         </div>
         {renderedUsers}
     </div>;
